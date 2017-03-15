9188aa38ef0b4debf031ae91035cfccb0b8be6bb
Amanda Furzer <amanda.furzer@P15017-AFurzer.local>
2017-03-15 10:39:36 -0400

    tests on arraryExt

--- test/arrayExt.test.js
+++ test/arrayExt.test.js
@@ -2,11 +2,86 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var assert = require("assert");
 require("../src/arrayExt");
-suite("remove", function () {
-    test("should pass", function () {
-        var collection = [1, 2, 3, 4, 5];
-        var take = collection.take(2);
-        assert.deepEqual(take.length, 2);
+suite("ArrayExt", function () {
+    var numbers;
+    var strings;
+    var objects;
+    setup(function () {
+        numbers = [2, 3, 1, 7];
+        strings = ["charlie", "alpha", "india", "bravo"];
+        objects = [
+            { item: "item4", value: 4 },
+            { item: "item1", value: 1 },
+            { item: "item3", value: 3 },
+            { item: "item2", value: 2 }
+        ];
+    });
+    var arrayEqual = function (actual, expected) {
+        if (actual === expected)
+            return true;
+        if (actual === null || expected === null)
+            return false;
+        if (!(actual instanceof Array) || !(expected instanceof Array))
+            return false;
+        if (actual.length !== expected.length)
+            return false;
+        for (var i = 0; i < actual.length; i++) {
+            if (actual[i] === expected[i])
+                continue;
+            return false;
+        }
+        return true;
+    };
+    suite("orderBy()", function () {
+        test("should return array of numbers in ascending order", function () {
+            assert.ok(arrayEqual(numbers.orderBy(), [1, 2, 3, 7]));
+        });
+        test("should return array of strings in ascending order", function () {
+            assert.ok(arrayEqual(strings.orderBy(), ["alpha", "bravo", "charlie", "india"]));
+        });
+        test.skip("should return array of objects in ascending order", function () {
+            assert.ok(arrayEqual(objects.orderBy(), [
+                { item: "item1", value: 1 },
+                { item: "item2", value: 2 },
+                { item: "item3", value: 3 },
+                { item: "item4", value: 4 }
+            ]));
+        });
+    });
+    suite("orderByDesc()", function () {
+        test("should return array of numbers in descending order", function () {
+            assert.ok(arrayEqual(numbers.orderByDesc(), [7, 3, 2, 1]));
+        });
+        test("should return array of strings in descending order", function () {
+            assert.ok(arrayEqual(strings.orderByDesc(), ["india", "charlie", "bravo", "alpha"]));
+        });
+        test.skip("should return array of objects in descending order", function () {
+        });
+    });
+    suite("skip()", function () {
+        test("should return remaining numbers in array after number skipped", function () {
+            assert.ok(arrayEqual(numbers.skip(2), [1, 7]));
+        });
+        test("should return remaining strings in array after number skipped", function () {
+            assert.ok(arrayEqual(strings.skip(2), ["india", "bravo"]));
+        });
+        test.skip("should return remaining objects in array after number skipped", function () {
+        });
+    });
+    suite("take()", function () {
+        test("should return remaining numbers in array after number taken", function () {
+            assert.ok(arrayEqual(numbers.take(2), [2, 3]));
+        });
+        test("should return remaining strings in array after number taken", function () {
+            assert.ok(arrayEqual(strings.take(2), ["charlie", "alpha"]));
+        });
+        test.skip("should return remaining objects in array after number taken", function () {
+        });
+    });
+    suite("count()", function () {
+        test.only("", function () {
+            console.log(numbers.count());
+        });
     });
 });
 //# sourceMappingURL=arrayExt.test.js.map
--- test/arrayExt.test.ts
+++ test/arrayExt.test.ts
@@ -1,16 +1,131 @@
 import * as assert from "assert";
 import "../src/arrayExt";
 
+suite("ArrayExt", () =>    
+{
+    let numbers: number[];
+    let strings: string[];
+    let objects: any[];
 
-suite("remove",
-    () =>
+    setup(() =>
     {
-        test("should pass",
-            () => 
-            {
-                let collection = [1, 2, 3, 4, 5];
-                let take = collection.take(2);
-                assert.deepEqual(take.length, 2);
-            });
+        numbers = [2, 3, 1, 7];
+        strings = ["charlie", "alpha", "india", "bravo"];
+        objects = [
+            { item: "item4", value: 4 },
+            { item: "item1", value: 1 },
+            { item: "item3", value: 3 },
+            { item: "item2", value: 2 }
+        ];
+    });
 
-    }); 
+    let arrayEqual = (actual: Array<any>, expected: Array<any>) =>
+    {
+        if (actual === expected)
+            return true;
+
+        if (actual === null || expected === null)
+            return false;
+
+        if (!(actual instanceof Array) || !(expected instanceof Array))
+            return false;
+
+        if (actual.length !== expected.length)
+            return false;
+
+        for (let i = 0; i < actual.length; i++)
+        {
+            if (actual[i] === expected[i])
+                continue;
+
+            return false;
+        }
+
+        return true;
+    };
+
+    suite("orderBy()", () =>
+    {
+        test("should return array of numbers in ascending order", () => 
+        {
+            assert.ok(arrayEqual(numbers.orderBy(), [1, 2, 3, 7]));
+        });
+
+        test("should return array of strings in ascending order", () =>
+        {
+            assert.ok(arrayEqual(strings.orderBy(), ["alpha", "bravo", "charlie", "india"]));
+        });
+
+        test.skip("should return array of objects in ascending order", () =>
+        {
+            assert.ok(arrayEqual(objects.orderBy(), [
+                { item: "item1", value: 1 },
+                { item: "item2", value: 2 },
+                { item: "item3", value: 3 },
+                { item: "item4", value: 4 }
+            ]));
+        });
+    });
+
+    suite("orderByDesc()", () =>
+    {
+        test("should return array of numbers in descending order", () =>
+        {
+            assert.ok(arrayEqual(numbers.orderByDesc(), [7, 3, 2, 1]));
+        });
+
+        test("should return array of strings in descending order", () =>
+        {
+            assert.ok(arrayEqual(strings.orderByDesc(), ["india", "charlie", "bravo", "alpha"]));
+        });
+
+        test.skip("should return array of objects in descending order", () =>
+        {
+
+        });
+    });
+
+    suite("skip()", () =>
+    {
+        test("should return remaining numbers in array after number skipped", () =>
+        {
+            assert.ok(arrayEqual(numbers.skip(2), [1, 7]));
+        });
+
+        test("should return remaining strings in array after number skipped", () =>
+        {
+            assert.ok(arrayEqual(strings.skip(2), ["india", "bravo"]));
+        });
+
+        test.skip("should return remaining objects in array after number skipped", () =>
+        {
+            
+        });     
+    });
+    
+    suite("take()", () =>
+    {
+        test("should return remaining numbers in array after number taken", () =>
+        {
+            assert.ok(arrayEqual(numbers.take(2), [2, 3])); 
+        });
+        
+        test("should return remaining strings in array after number taken", () =>
+        {
+            assert.ok(arrayEqual(strings.take(2), ["charlie", "alpha"]));
+        });
+        
+        test.skip("should return remaining objects in array after number taken", () =>
+        {
+            
+        });
+    });
+    
+    suite("count()", () =>
+    {
+        test.only("", () =>
+        {
+            console.log(numbers.count());
+        }); 
+    });
+});
