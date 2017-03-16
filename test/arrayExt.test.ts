ebf007d1cfb9ec6e4d26e2305cf56529e835ca3c
Amanda Furzer <amanda.furzer@P15017-AFurzer.local>
2017-03-16 16:59:01 -0400

    implemented remaining tests on arrayExt

--- test/arrayExt.test.ts
+++ test/arrayExt.test.ts
@@ -1,21 +1,29 @@
 import * as assert from "assert";
 import "../src/arrayExt";
 
-suite("ArrayExt", () =>    
+suite.only("ArrayExt", () =>    
 {
     let numbers: number[];
     let strings: string[];
+    
     let objects: any[];
+    let first = { item: "item1", value: 1 };
+    let second = { item: "item2", value: 2 };
+    let third = { item: "item3", value: 3 };
+    let fourth = { item: "item4", value: 4 };
+    
 
     setup(() =>
     {
         numbers = [2, 3, 1, 7];
         strings = ["charlie", "alpha", "india", "bravo"];
+        
+        
         objects = [
-            { item: "item4", value: 4 },
-            { item: "item1", value: 1 },
-            { item: "item3", value: 3 },
-            { item: "item2", value: 2 }
+            fourth,
+            first,
+            third,
+            second
         ];
     });
 
@@ -44,7 +52,7 @@ suite("ArrayExt", () =>
         return true;
     };
 
-    suite("orderBy()", () =>
+    suite("orderBy", () =>
     {
         test("should return array of numbers in ascending order", () => 
         {
@@ -56,18 +64,14 @@ suite("ArrayExt", () =>
             assert.ok(arrayEqual(strings.orderBy(), ["alpha", "bravo", "charlie", "india"]));
         });
 
-        test.skip("should return array of objects in ascending order", () =>
+        test("should return array of objects in ascending order", () =>
         {
-            assert.ok(arrayEqual(objects.orderBy(), [
-                { item: "item1", value: 1 },
-                { item: "item2", value: 2 },
-                { item: "item3", value: 3 },
-                { item: "item4", value: 4 }
-            ]));
+            let ordered = objects.orderBy(t => t.item);
+            assert.ok(arrayEqual(ordered, [first, second, third, fourth]));
         });
     });
 
-    suite("orderByDesc()", () =>
+    suite("orderByDesc", () =>
     {
         test("should return array of numbers in descending order", () =>
         {
@@ -79,35 +83,47 @@ suite("ArrayExt", () =>
             assert.ok(arrayEqual(strings.orderByDesc(), ["india", "charlie", "bravo", "alpha"]));
         });
 
-        test.skip("should return array of objects in descending order", () =>
+        test("should return array of objects in descending order", () =>
         {
-
+            let ordered = objects.orderByDesc(t => t.value);
+            assert.ok(arrayEqual(ordered, [fourth, third, second, first]));
         });
     });
 
-    suite("skip()", () =>
+    suite("skip", () =>
     {
-        test("should return remaining numbers in array after number skipped", () =>
+        test("should return remaining numbers in array after number of elements skipped", () =>
         {
             assert.ok(arrayEqual(numbers.skip(2), [1, 7]));
         });
+        
+        test("should return empty array if number skipped is > array.length", () =>
+        {
+            assert.ok(arrayEqual(numbers.skip(7), []));
+        });
 
-        test("should return remaining strings in array after number skipped", () =>
+        test("should return remaining strings in array after number of elements skipped", () =>
         {
             assert.ok(arrayEqual(strings.skip(2), ["india", "bravo"]));
         });
 
-        test.skip("should return remaining objects in array after number skipped", () =>
+        test("should return remaining objects in array after number of elements skipped", () =>
         {
-            
+            let skipped = objects.skip(2);
+            assert.ok(arrayEqual(skipped, [third, second]));
         });     
     });
     
-    suite("take()", () =>
+    suite("take", () =>
     {
         test("should return remaining numbers in array after number taken", () =>
         {
             assert.ok(arrayEqual(numbers.take(2), [2, 3])); 
+        });
+        
+        test("should return complete array if number taken is > array.length", () =>
+        {
+            assert.ok(arrayEqual(numbers.take(6), [2, 3, 1, 7]));
         });
         
         test("should return remaining strings in array after number taken", () =>
@@ -115,17 +131,73 @@ suite("ArrayExt", () =>
             assert.ok(arrayEqual(strings.take(2), ["charlie", "alpha"]));
         });
         
-        test.skip("should return remaining objects in array after number taken", () =>
+        test("should return remaining objects in array after number taken", () =>
         {
-            
+            let taken = objects.take(2);
+            assert.ok(arrayEqual(taken, [fourth, first]));
         });
     });
     
-    suite("count()", () =>
+    suite("count", () =>
     {
-        test.only("", () =>
+        test("should return the length of the array when called without a predicate", () =>
         {
-            console.log(numbers.count());
-        }); 
+            let count = numbers.count();
+            assert.strictEqual(count, numbers.length);
+        });
+        
+        test("should return number of items that satisfy the predicate condition when called with a predicate", () =>
+        {
+            let count = numbers.count(t => t > 5);
+            assert.strictEqual(count, 1);
+        });
+    });
+    
+    suite("remove", () =>
+    {
+        test("should return array of numbers minus removed element", () =>
+        {
+            numbers.remove(3);
+            assert.ok(arrayEqual(numbers, [2, 1, 7]));     
+        });
+        
+        test("should return array of strings minus removed element", () =>
+        {
+            strings.remove("alpha");
+            assert.ok(arrayEqual(strings, ["charlie", "india", "bravo"]));
+        });
+        
+        test("should return array of objects minus removed element", () =>
+        {
+            objects.remove(first);
+            assert.ok(arrayEqual(objects, [fourth, third, second]));
+        });
+        
+        test("should return false if element is not in array", () =>
+        {
+            let removed = numbers.remove(8);
+            assert.strictEqual(removed, false);
+        });
+    });
+    
+    suite("clear", () =>
+    {
+        test("should remove all elements in array of numbers", () =>
+        {
+            numbers.clear();
+            assert.ok(arrayEqual(numbers, []));
+        });
+        
+        test("should remove all elements in array of strings", () =>
+        {
+            strings.clear();
+            assert.ok(arrayEqual(strings, []));
+        });
+        
+        test("should remove all elements in array of objects", () =>
+        {
+            objects.clear();
+            assert.ok(arrayEqual(objects, []));
+        });
     });
 });
