ff05156dda2e69d4503d8d2be5f605e55277319e
nivinjoseph <nivinjoseph@outlook.com>
2017-03-21 19:04:32 -0400

    refactorings

--- dist/stringExt.d.ts
+++
@@ -1,8 +0,0 @@
-declare class StringExt {
-    static isEmptyOrWhiteSpace(value: string): boolean;
-    static contains(primary: string, sub: string): boolean;
-    static startsWith(primary: string, sub: string): boolean;
-    static endsWith(primary: string, sub: string): boolean;
-    static extractNumbers(value: string): string;
-    static format(formatString: string, ...params: any[]): string;
-}
