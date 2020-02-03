d85a81a274acb18bfc212451ba315ea7a5ad04c2
nivinjoseph <nivinjoseph@outlook.com>
2020-02-02 23:17:51 -0500

    signature changes in array-ext

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -56,21 +56,21 @@ declare global
     interface ReadonlyArray<T>
     {
         contains(value: T): boolean;
-        orderBy(): ReadonlyArray<T>;
-        orderBy(compareFunc: (value: T) => any): ReadonlyArray<T>;
-        orderByDesc(): ReadonlyArray<T>;
-        orderByDesc(compareFunc: (value: T) => any): ReadonlyArray<T>;
-        groupBy(keyFunc: (value: T) => string): ReadonlyArray<{ key: string, values: ReadonlyArray<T> }>
-        distinct(): ReadonlyArray<T>;
-        distinct(compareFunc: (value: T) => any): ReadonlyArray<T>;
-        skip(count: number): ReadonlyArray<T>;
-        take(count: number): ReadonlyArray<T>;
+        orderBy(): Array<T>;
+        orderBy(compareFunc: (value: T) => any): Array<T>;
+        orderByDesc(): Array<T>;
+        orderByDesc(compareFunc: (value: T) => any): Array<T>;
+        groupBy(keyFunc: (value: T) => string): Array<{ key: string, values: Array<T> }>
+        distinct(): Array<T>;
+        distinct(compareFunc: (value: T) => any): Array<T>;
+        skip(count: number): Array<T>;
+        take(count: number): Array<T>;
         count(): number;
         count(predicate: (value: T) => boolean): number;
         equals(compareArray: ReadonlyArray<T>): boolean;
         equals(compareArray: ReadonlyArray<T>, compareFunc: (t1: T, t2: T) => boolean): boolean;
         forEachAsync(asyncFunc: (input: T) => Promise<void>, degreesOfParallelism?: number): Promise<void>;
-        mapAsync<U>(asyncFunc: (input: T) => Promise<U>, degreesOfParallelism?: number): Promise<ReadonlyArray<U>>;
+        mapAsync<U>(asyncFunc: (input: T) => Promise<U>, degreesOfParallelism?: number): Promise<Array<U>>;
         reduceAsync<U>(asyncFunc: (acc: U, input: T) => Promise<U>, accumulator?: U): Promise<U>;
     }   
     
