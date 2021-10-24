1c6fd1a058872d3ead935e465eaddcff778263c0
nivinjoseph <nivinjoseph@outlook.com>
2021-10-24 09:45:12 -0400

    preparing to publish new version

--- dist/array-ext.js
+++ dist/array-ext.js
@@ -457,29 +457,59 @@ function defineArrayExtProperties() {
                 return this.length > 0;
             }
         });
+    // // @ts-ignore
+    // if (Array.prototype["first"] === undefined)
+    //     Object.defineProperty(Array.prototype, "first", {
+    //         configurable: false,
+    //         enumerable: false,
+    //         get: function ()
+    //         {
+    //             // if (this.length === 0)
+    //             //     throw new Error("Invalid Operation: Array is empty");
+    //             if (this.length === 0)
+    //                 return undefined;
+    //             return this[0];
+    //         }
+    //     });
     // @ts-ignore
-    if (Array.prototype["first"] === undefined)
-        Object.defineProperty(Array.prototype, "first", {
+    if (Array.prototype["takeFirst"] === undefined)
+        Object.defineProperty(Array.prototype, "takeFirst", {
             configurable: false,
             enumerable: false,
-            get: function () {
-                // if (this.length === 0)
-                //     throw new Error("Invalid Operation: Array is empty");
+            writable: false,
+            value: function () {
                 if (this.length === 0)
-                    return undefined;
+                    throw new Error("Invalid Operation: Array is empty");
+                // if (this.length === 0)
+                //     return undefined;
                 return this[0];
             }
         });
+    // // @ts-ignore
+    // if (Array.prototype["last"] === undefined)
+    //     Object.defineProperty(Array.prototype, "last", {
+    //         configurable: false,
+    //         enumerable: false,
+    //         get: function ()
+    //         {
+    //             if (this.length === 0)
+    //                 throw new Error("Invalid Operation: Array is empty");
+    //             // if (this.length === 0)
+    //             //     return undefined;
+    //             return this[this.length - 1];
+    //         }
+    //     });
     // @ts-ignore
-    if (Array.prototype["last"] === undefined)
-        Object.defineProperty(Array.prototype, "last", {
+    if (Array.prototype["takeLast"] === undefined)
+        Object.defineProperty(Array.prototype, "takeLast", {
             configurable: false,
             enumerable: false,
-            get: function () {
-                // if (this.length === 0)
-                //     throw new Error("Invalid Operation: Array is empty");
+            writable: false,
+            value: function () {
                 if (this.length === 0)
-                    return undefined;
+                    throw new Error("Invalid Operation: Array is empty");
+                // if (this.length === 0)
+                //     return undefined;
                 return this[this.length - 1];
             }
         });
